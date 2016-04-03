2f8366ab967bc2011212733e4a2a8a3e5156fca6
kushalchawla <kushalchawla18@gmail.com>
2016-04-03 16:47:01 +0530

    all done except create_buckets func.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream> 
 #include <cstring>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -93,7 +94,8 @@ int get_no_of_rec(int cur_round, string cur_bucket,int relation_no)
   	return count;
 }
 
-void print_size(int cur_round, string cur_bucket,int relation_no)
+//returns size in pages
+int get_size(int cur_round, string cur_bucket,int relation_no)
 {
 	int no_of_records,size_in_pages;
 	
@@ -109,7 +111,56 @@ void print_size(int cur_round, string cur_bucket,int relation_no)
 		size_in_pages++;
 	}
 	
-	f_log<<"Size of relation "<<relation_no<<": "<<size_in_pages<<" pages\n";
+	return size_in_pages;
+}
+
+//get file data in a vector.
+vector<int> get_data(int cur_round, string cur_bucket,int relation_no)
+{
+	ifstream ifs;
+
+	string s=to_string(relation_no);
+	char *ss=new char[s.size()+1];
+	ss[s.size()]=0;
+	memcpy(ss,s.c_str(),s.size());
+
+	char str[]="relation";
+	if(cur_round==1)
+	{
+		strcat(str,ss);
+		strcat(str,".txt");
+	}
+	else
+	{
+		strcat(str,ss);
+
+		string s1=to_string(cur_round);
+		char *rd=new char[s1.size()+1];
+		rd[s1.size()]=0;
+		memcpy(rd,s1.c_str(),s1.size());
+
+		string s2=cur_bucket;
+		char *buck=new char[s2.size()+1];
+		buck[s2.size()]=0;
+		memcpy(buck,s2.c_str(),s2.size());
+
+		strcat(str,".round");
+		strcat(str,rd);
+		strcat(str,".bucket");
+		strcat(str,buck);
+		strcat(str,".txt");
+	}
+	ifs.open(str);
+	std::vector<int> v;
+	int val;
+	while(ifs.good())
+	{
+		ifs>>val;
+		v.push_back(val);
+	}
+
+	return v;
+
 }
 
 void update_log(int cur_round,string cur_bucket)
@@ -122,15 +173,82 @@ void update_log(int cur_round,string cur_bucket)
 
 		Hashing round and bucket no --
 	*/
+	int size_in_pages;
+	size_in_pages=get_size(cur_round,cur_bucket,1);
+	f_log<<"Size of relation 1: "<<size_in_pages<<" pages\n";
 
-	print_size(cur_round,cur_bucket,1);
-	print_size(cur_round,cur_bucket,2);
+	size_in_pages=get_size(cur_round,cur_bucket,2);
+	f_log<<"Size of relation 2: "<<size_in_pages<<" pages\n";
+
 	f_log<<"Total number of available pages: "<<avail_pages<<endl;
 	f_log<<"\nHashing round no: "<<cur_round<<" , Hashing bucket no: "<<cur_bucket<<endl;
 	f_log<<"\n\n";
 }
 
+void update_result(int cur_round, string cur_bucket)
+{
 
+	int size1=get_size(cur_round,cur_bucket,1);
+	int size2=get_size(cur_round,cur_bucket,2);
+
+	int tot_size=size1+size2;
+	f_result<<"\nHashing round no: "<<cur_round<<"\nHashing bucket no: "<<cur_bucket<<endl;
+	f_result<<"Total size is "<<tot_size<<" pages.\n";
+	f_result<<"Total available pages are"<<avail_pages<<" pages.\n";
+}
+
+//find if 2 files can be joined in the main memory
+bool can_be_joined(int cur_round, string cur_bucket)
+{
+	int size1=get_size(cur_round,cur_bucket,1);
+	int size2=get_size(cur_round,cur_bucket,2);
+
+	if((size1+size2)<avail_pages)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+//join 2 files in main memory - needs to checked if possible before by calling can_be_joined
+void join_them(int cur_round, string cur_bucket,vector<int> vec1, vector<int> vec2)
+{
+	int i,j,len1=vec1.size();
+	int len2=vec2.size();
+
+	update_result(cur_round,cur_bucket);
+	f_result<<"Performing in memory join:\nMatching pairs are: \n";
+
+	int ff=0;
+
+	for(i=1;i<len1;i++)
+	{
+		for(j=1;j<len2;j++)
+		{
+			if(vec1[i]==vec2[j])
+			{
+				ff=1;
+				f_result<<vec1[i]<<" , "<<vec1[i]<<endl;
+				break;
+			}
+		}
+	}
+	if(ff==0)
+	{
+		f_result<<"..No matching tuples found!..\n";
+	}
+
+}
+
+//create buckets for both the files and fill it into corresponding files  
+void create_buckets(int cur_round, string cur_bucket)
+{
+
+}
 
 /*
 	solve the work	
@@ -148,27 +266,36 @@ void update_log(int cur_round,string cur_bucket)
 */
 void solver(int cur_round, string cur_bucket)
 {
-	if(final_flag=0)
-	{
-		return;
-	}
-
 	update_log(cur_round,cur_bucket);
 	
-	if(cur_round>max_h_rounds)
+	std::vector<int> vec1;
+	std::vector<int> vec2;
+
+	vec1=get_data(cur_round,cur_bucket,1);
+	vec2=get_data(cur_round,cur_bucket,2);
+
+	int size1=vec1.size();
+	int size2=vec2.size();
+
+	if(size1==0 || size2==0)
 	{
-		final_flag=0;
-		cout<<"Maximum rounds reached! Cannot perform join at bucket : "<<cur_bucket<<endl;
-		f_log<<"Maximum rounds reached! Cannot perform join at bucket : "<<cur_bucket<<endl;
+		update_result(cur_round,cur_bucket);
+		f_result<<"No matching tuple. No further processing required!\n";
 		return;
 	}
-
 	if(can_be_joined(cur_round,cur_bucket))
 	{
-		join_them(cur_round,cur_bucket);
+		join_them(cur_round,cur_bucket,vec1,vec2);
 	}
 	else
 	{
+		if(cur_round==max_h_rounds)
+		{
+			final_flag=0;
+			cout<<"Maximum rounds reached! Cannot perform join at bucket : "<<cur_bucket<<endl;
+			f_log<<"Maximum rounds reached! Cannot perform join at bucket : "<<cur_bucket<<endl;
+			return;
+		}
 		//create buckets for both files having a file corresponding to each bucket!
 		create_buckets(cur_round,cur_bucket);
 		
